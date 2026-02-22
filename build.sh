07e827ef88754dfafe70fa005182cafef6b659fb
cmsimmons98-eng <cmsimmons98@gmail.com>
2026-02-22 14:05:22 -0500

    trying to github this properly again. Edited the build.sh file

--- build.sh
+++ build.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-
-# Replace YOUR_GITHUB_USERNAME and YOUR_REPO_NAME below
-python3 src/main.py "/cmsimmons98-eng.github.io/static_site_generator/"
+python3 src/main.py "/static_site_generator/"
